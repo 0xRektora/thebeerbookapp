c6734bf575d0c5a8dd6bf0309475e2446b5f2108
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-27 18:55:05 -0500

    feat: improvements & refactor
    + BeerCard => Refactored + Updated styling and classes
    + BeerDeck => Refactored + impl beer deck
    vote behaviors

--- src/shared/components/BeerCard.tsx
+++ src/shared/components/BeerCard.tsx
@@ -1,14 +1,16 @@
+import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 import { animated, interpolate } from 'react-spring';
 import { ReactEventHandlers } from 'react-use-gesture/dist/types';
+import { IBeerModel } from '../models/IBeerModel';
+import { Utils } from '../utils/Utils';
 
 const useStyles = makeStyles({
     container: {
-        backgroundColor: 'white',
-        backgroundSize: 'auto 85%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
+        background: 'white',
+        paddingTop: 10,
+        paddingBottom: 10,
         width: '45vh',
         maxWidth: '300px',
         height: '85vh',
@@ -17,18 +19,39 @@ const useStyles = makeStyles({
         borderRadius: '10px',
         boxShadow:
             '0 12.5px 100px -10px rgba(50, 50, 73, 0.4), 0 10px 10px -10px rgba(50, 50, 73, 0.3)',
+        cursor: 'pointer',
+    },
+    img_container: {
+        backgroundColor: 'white',
+        height: '100%',
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+        maxHeight: 'calc(100vh - 25vh)',
+        pointerEvents: 'none',
+    },
+    details: {
+        padding: '5%',
+    },
+    details_title: {
+        fontStyle: 'italic',
+        fontSize: 'larger',
     },
 });
 
 export interface IBeerCardProps {
-    rot: number;
+    rotation: number;
     scale: number;
     index: number;
-    img: string;
+    disableWoodBackground?: boolean;
     bind?: (...args: any[]) => ReactEventHandlers;
+    data?: IBeerModel;
 }
 
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
+/**
+ *  This is being used down there in the view, it interpolates rotation and scale into a css transform
+ **/
 const trans = (r: number, s: any) =>
     `perspective(1500px) rotateX(30deg) rotateY(${
         r / 10
@@ -37,6 +60,60 @@ const trans = (r: number, s: any) =>
 export function BeerCard(props: IBeerCardProps) {
     const classes = useStyles();
 
+    function renderImg() {
+        return (
+            <div
+                className={classes.img_container}
+                style={{
+                    ...(props.disableWoodBackground
+                        ? {}
+                        : {
+                              background:
+                                  'url("https://images.unsplash.com/photo-1589874497556-65019ae69438?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80") no-repeat center',
+                          }),
+                }}
+            >
+                <img
+                    className={classes.img}
+                    src={props.data?.imageUrl}
+                    alt={props.data?.imageUrl ?? 'Blank'}
+                />
+            </div>
+        );
+    }
+
+    function renderContent() {
+        return (
+            <Grid
+                container
+                alignItems="center"
+                style={{ height: '100%', flexFlow: 'column' }}
+                spacing={1}
+            >
+                <Grid item>
+                    <strong>
+                        {Utils.formatAlcoholPercentage(
+                            props.data?.percentage ?? 0,
+                        )}
+                    </strong>
+                </Grid>
+
+                <Grid item style={{ height: '100%' }}>
+                    {renderImg()}
+                </Grid>
+
+                <Grid item style={{ textAlign: 'center' }}>
+                    <strong className={classes.details_title}>
+                        {props.data?.name}
+                    </strong>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    /**
+     * Will render conditionally a gesture bound card, or a simple div with the proper data
+     */
     function bindExist() {
         if (props.bind) {
             return (
@@ -45,21 +122,17 @@ export function BeerCard(props: IBeerCardProps) {
                     {...(props.bind ? props.bind(props.index) : {})}
                     className={classes.container}
                     style={{
-                        transform: interpolate([props.rot, props.scale], trans),
-                        backgroundImage: `url(${props.img})`,
+                        transform: interpolate(
+                            [props.rotation, props.scale],
+                            trans,
+                        ),
                     }}
-                />
+                >
+                    {renderContent()}
+                </animated.div>
             );
         } else {
-            return (
-                <div
-                    className={classes.container}
-                    style={{
-                        transform: trans(props.rot, props.scale),
-                        backgroundImage: `url(${props.img})`,
-                    }}
-                ></div>
-            );
+            return <div className={classes.container}>{renderContent()}</div>;
         }
     }
     return bindExist();
--- src/shared/components/BeerDeck.tsx
+++ src/shared/components/BeerDeck.tsx
@@ -1,17 +1,14 @@
 import { makeStyles } from '@material-ui/styles';
-import React, { useState } from 'react';
-import { useSprings, animated, interpolate } from 'react-spring';
+import React, { useRef, useState } from 'react';
+import { animated, interpolate, useSprings } from 'react-spring';
 import { useGesture } from 'react-use-gesture';
+import {
+    CommonGestureState,
+    Coordinates,
+    SharedGestureState,
+} from 'react-use-gesture/dist/types';
+import { IBeerModel } from '../models/IBeerModel';
 import { BeerCard } from './BeerCard';
-
-const cards = [
-    'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg',
-    'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg',
-    'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg',
-    'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-    'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-    'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-];
 
 const useStyles = makeStyles({
     container: {
@@ -26,6 +23,7 @@ const useStyles = makeStyles({
 });
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
+// Work as the "loading" state
 const to = (i: number) => ({
     x: 0,
     y: i * -4,
@@ -34,64 +32,95 @@ const to = (i: number) => ({
     delay: i * 100,
 });
 const from = (i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r: number, s: any) =>
-    `perspective(1500px) rotateX(30deg) rotateY(${
-        r / 10
-    }deg) rotateZ(${r}deg) scale(${s})`;
 
-export function BeerDeck() {
+export interface IBeerDeckProps {
+    cards: IBeerModel[];
+    disableWoodBackground?: boolean;
+    onDeckIsEmpty?: () => void;
+    onVoteOccurs?: (index: number, voteType: 'left' | 'right') => void;
+}
+
+export function BeerDeck(props: IBeerDeckProps) {
     const classes = useStyles();
     const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
-    const [props, set] = useSprings(cards.length, (i) => ({
+
+    const springRef = useRef<any>();
+    const [springProps, set] = useSprings(props.cards.length, (i) => ({
         ...to(i),
         from: from(i),
+        ref: i === props.cards.length - 1 ? springRef : undefined,
     })); // Create a bunch of springs using the helpers above
-    // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-    const bind = useGesture(
-        ({
-            args: [index],
-            down,
-            delta: [xDelta],
-            distance,
-            direction: [xDir],
-            velocity,
-        }) => {
-            const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-            const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-            if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
 
+    // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+    const bind = useGesture(handleGesture);
+
+    /**
+     *  Handle the gestures for the cards.
+     *
+     *  Scale on click, X axis movements support, velocity handler
+     * @param [CommonGestureState & Coordinates & SharedGestureState] react useGesture params
+     */
+    function handleGesture({
+        args: [index],
+        down,
+        delta: [xDelta],
+        distance,
+        direction: [xDir],
+        velocity,
+    }: CommonGestureState & Coordinates & SharedGestureState) {
+        const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
+        const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
+        if (!down && trigger) {
+            gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+            if (props.onVoteOccurs) {
+                props.onVoteOccurs(index, dir === 1 ? 'right' : 'left');
+            }
+        }
+
+        //@ts-ignore
+        set((i: number) =>
+            updateCardAnimationOnSwipe(i, index, dir, down, velocity, xDelta),
+        );
+        if (!down && gone.size === props.cards.length) onDeckIsEmpty();
+    }
+
+    function updateCardAnimationOnSwipe(
+        i: number,
+        index: number,
+        dir: -1 | 1,
+        down: boolean,
+        velocity: number,
+        xDelta: number,
+    ) {
+        if (index !== i) return; // We're only interested in changing spring-data for the current spring
+        const isGone = gone.has(index);
+        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
+        const scale = down ? 1.1 : 1; // Active cards lift up a bit
+        return {
+            x,
+            rot,
+            scale,
+            delay: undefined,
+            config: {
+                friction: 50,
+                tension: down ? 800 : isGone ? 200 : 500,
+            },
+        };
+    }
+
+    function onDeckIsEmpty() {
+        setTimeout(() => {
+            if (props.onDeckIsEmpty) props.onDeckIsEmpty();
+            gone.clear();
             //@ts-ignore
-            set((i: any) => {
-                if (index !== i) return; // We're only interested in changing spring-data for the current spring
-                const isGone = gone.has(index);
-                const x = isGone
-                    ? (200 + window.innerWidth) * dir
-                    : down
-                    ? xDelta
-                    : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-                const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-                const scale = down ? 1.1 : 1; // Active cards lift up a bit
-                return {
-                    x,
-                    rot,
-                    scale,
-                    delay: undefined,
-                    config: {
-                        friction: 50,
-                        tension: down ? 800 : isGone ? 200 : 500,
-                    },
-                };
-            });
-            if (!down && gone.size === cards.length)
-                //@ts-ignore
-                setTimeout(() => gone.clear() || set((i: any) => to(i)), 600);
-        },
-    );
-    // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-    return (
-        <>
-            {props.map(({ x, y, rot, scale }, i) => (
+            set((i: number) => to(i));
+        }, 600);
+    }
+
+    function renderDeck() {
+        return springProps.map(({ x, y, rot, scale }, i) => {
+            return (
                 <animated.div
                     className={classes.container}
                     key={i}
@@ -105,12 +134,16 @@ export function BeerDeck() {
                     <BeerCard
                         bind={bind}
                         index={i}
-                        rot={rot}
+                        rotation={rot}
                         scale={scale}
-                        img={cards[i]}
+                        data={props.cards[i]}
+                        disableWoodBackground={props.disableWoodBackground}
                     />
                 </animated.div>
-            ))}
-        </>
-    );
+            );
+        });
+    }
+
+    // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+    return <>{renderDeck()}</>;
 }
