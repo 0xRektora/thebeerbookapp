ba6ca9014ccf0e20f7c0d0702e7d7621eb291b8d
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-27 18:56:38 -0500

    chore(app entrypoint): added global providers, updated index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { BeerCard } from './shared/components/BeerCard';
-import { BeerDeck } from './shared/components/BeerDeck';
+import BeerVote from './services/BeerVote/BeerVote';
 
 function App() {
     return (
         <div className="App">
-            {/* <BeerDeck /> */}
-            <BeerCard
-                index={1}
-                rot={36}
-                scale={36}
-                img="https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg"
-            />
+            <BeerVote />
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'fontsource-roboto';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { SnackbarProvider } from 'notistack';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <SnackbarProvider maxSnack={2}>
+                <App />
+            </SnackbarProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
