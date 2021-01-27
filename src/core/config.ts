e0df252efe5b98ef3fe32c2ef3af9f3d63bfe808
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-27 16:30:06 -0500

    feat(env/conf): env vars handling
    + Added .env* files
    + Added config ts file
    + Typed process.env

---
+++ src/core/config.ts
@@ -0,0 +1,10 @@
+const THE_BEER_BOOK_ROUTE =
+    process.env.REACT_APP_THE_BEER_BOOK_ROUTE ?? 'localhost:4000/api';
+
+const CONFIG = {
+    api: {
+        THE_BEER_BOOK_ROUTE,
+    },
+};
+
+export default CONFIG;
---
+++ src/core/environment.d.ts
@@ -0,0 +1,9 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            REACT_APP_THE_BEER_BOOK_ROUTE;
+        }
+    }
+}
+
+export {};
