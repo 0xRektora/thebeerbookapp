eaf8dd956e7733bf1b76960bc64df734baa1d144
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-01-28 12:55:16 -0500

    style(colors): updated app bg color and added colors to config obj

--- src/core/config.ts
+++ src/core/config.ts
@@ -5,6 +5,12 @@ const CONFIG = {
     api: {
         THE_BEER_BOOK_ROUTE,
     },
+    palette: {
+        primary: '#fae637',
+        secondary: '#5d1cb0',
+        blue: '#3875e7',
+        red: '#f1114f',
+    },
 };
 
 export default CONFIG;
